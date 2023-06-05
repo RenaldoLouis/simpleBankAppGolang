c76407b53884e46b9f8287a2ea65628f69b03593
Renaldo Louis <renaldo.louis@commsult.id>
2023-06-05 16:14:03 +0700

    add test for user database and handle db error

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,10 +11,10 @@ import (
 )
 
 func createRandomAccount(t *testing.T) Account {
-	// user := createRandomUser(t)
+	user := createRandomUser(t)
 
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
