e78fece49b3b42e44cdbbee76d09149c11daecdf
Renaldo Louis <renaldo.louis@commsult.id>
2023-06-12 15:37:22 +0700

    craeate new API to add user and hash password

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,11 +10,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
-	// user := createRandomUser(t)
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
 
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
